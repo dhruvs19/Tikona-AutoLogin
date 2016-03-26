61ab167cc7ce04bf6aab6b75b7fd9a849ecbb5d6
Dhruv Sharma <dhruvsharma1904@gmail.com>
2016-03-26 22:35:06 +0530

    extracted url

--- url.py
+++ url.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
 import requests
+import sys
+import re
 
 url = requests.get("http://1.254.254.254")
 
-print url.text
-input
+
+statuscode = url.status_code
+
+if statuscode == 200:
+	print "page received sucessfully"
+else:
+	print "Error Loading the page.. Exiting"
+	sys.exit(0)
+
+arr = re.search('requesturi.*',url.text).group(0)
+	
+print 'https://login.tikona.in/userportal/?'+arr[0:-2]
+
